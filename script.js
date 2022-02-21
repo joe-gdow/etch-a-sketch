c37de288ff59a2ce0e38c1c781cdc9b998ae2eef
joe <joe.gdowik@gmail.com>
2022-02-21 17:40:41 -0500

    added button input, css hover animation, trying to figure out how to get these squares into a grid

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const container = document.querySelector(".container");
-let gridSize = prompt("Enter grid size");
-gridSize = Math.floor(gridSize/2);
+const container = document.querySelector(".grid-container");
+const squareButton = document.getElementById('makeASquare');
+squareButton.addEventListener('click', printGrid);
 
+function clearPage() {
+    container.innerHTML = "";
+}
+
+function getValue() {
+    let value = document.getElementById('userInput').value;
+    if (value > 10) {
+        value = 10;
+    }
+    return value;
+}
 
 function addDiv() {
     let newDiv = document.createElement("div");
@@ -9,9 +20,14 @@ function addDiv() {
     container.appendChild(newDiv);
 }
 
-let i = 0;
-while (i<gridSize) {
-    addDiv();
-    i++;
+function printGrid() {
+    clearPage();
+    let gridSize = getValue();
+    let i = 0;
+    
+    while (i < gridSize*2) {
+        addDiv();
+        i++;
+    }
 }
 
