d75e78e26b237559900842bac0b076f4db481853
joe <joe.gdowik@gmail.com>
2022-02-22 21:26:58 -0500

    ok, finally got the square divs to scale according to the amount called for.

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-const container = document.querySelector(".grid-container");
+const container = document.querySelector('.grid-container'); 
 const squareButton = document.getElementById('makeASquare');
-squareButton.addEventListener('click', printGrid);
+squareButton.addEventListener('click', function(){
+    clearPage();
+    let size = getValue();
+    let i = 0;
+    while (i < size*size) {
+        addDiv();
+        i++;
+    }
+    container.style.gridTemplateColumns = `repeat(${size}, ${size}px)`;
+    let squareDiv = document.getElementsByClassName('square');
+    console.log(squareDiv);
+    let j = 0;
+    while (j < squareDiv.length) {
+        squareDiv[j].style.width = `${size}px`;
+        squareDiv[j].style.height = `${size}px`;
+        j++;
+    }
+});
 
 function clearPage() {
     container.innerHTML = "";
 }
 
 function getValue() {
-    let value = document.getElementById('userInput').value;
-    if (value > 10) {
-        value = 10;
+    value = document.getElementById('userInput').value;
+    if (value > 100) {
+        value = 100;
     }
     return value;
 }
@@ -20,14 +37,20 @@ function addDiv() {
     container.appendChild(newDiv);
 }
 
+/* function setSize() {
+    let gridSize = getValue();
+    document.querySelector('.square').style.width = gridSize + 'px;';
+    document.querySelector('.square').style.height = gridSize + 'px;';
+} */
+
 function printGrid() {
     clearPage();
-    let gridSize = getValue();
+    let size = getValue();
     let i = 0;
-    
-    while (i < gridSize*2) {
+    while (i < size*size) {
         addDiv();
         i++;
     }
+    container.style.gridTemplateColumns = 'repeat(' + size + ', ' + size + 'px);';
+    //setSize();
 }
-
